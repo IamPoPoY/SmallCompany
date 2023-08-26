64bd6482feb66eaa4da6f98b6a133f1774188ced
IamPoPoY <khayrullayev165@gmail.com>
2023-08-27 00:17:07 +0500

    spring data rest

--- src/main/java/com/example/smallcompany/repositary/AddressRepository.java
+++ src/main/java/com/example/smallcompany/repositary/AddressRepository.java
@@ -3,9 +3,13 @@ import com.example.smallcompany.entity.Address;
 import com.example.smallcompany.projection.CastomAdress;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 @RepositoryRestResource(path = "address",excerptProjection = CastomAdress.class)
 public interface AddressRepository extends JpaRepository<Address,Integer> {
-
+    @RestResource(path = "byStreet")
+    public List<Address> findAllByStreet(String street);
 
 }
